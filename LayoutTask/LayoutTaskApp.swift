29d772bad7a45725c7485c04e4d092f855a9a24a
MrCronkite <vladshima99@gmail.com>
2024-03-25 23:10:21 +0300

    Initial Commit

---
+++ LayoutTask/LayoutTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutTaskApp.swift
+//  LayoutTask
+//
+//  Created by Vladislav Shimchenko on 25.03.24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
